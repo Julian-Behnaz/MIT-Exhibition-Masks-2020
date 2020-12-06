c26f991a82b7579b5953405e61d52278e7e48706
behnaz <farahibo@gmail.com>
2020-12-06 15:26:40 -0800

    initial commit

---
+++ src/common.ts
@@ -0,0 +1,35 @@
+import { WebGLRenderer } from "three";
+
+export interface Hall {
+    name: string,
+    introId: string | null,
+    setup(): Promise<void>,
+    onEnter(renderer: WebGLRenderer): void,
+    onLeave(): void,
+    render(renderer: WebGLRenderer): void,
+    teardown(): Promise<void>,
+    resize(): void,
+    getProgressFrac(): number,
+}
+
+export enum HallState {
+    None,
+    Init,
+    StartedLoadingHall,
+    LoadingHall,
+    WaitingToEnterHall,
+    InHall,
+    StartedLeavingHall,
+    LeavingHall
+}
+
+export interface Halls {
+    renderer: WebGLRenderer,
+    state: HallState
+    currHallIdx: number
+    lastState: HallState,
+    lastHallIdx: number,
+    nextState: HallState,
+    nextHallIdx: number,
+    allHalls: Hall[],
+}
